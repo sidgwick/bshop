12dccfcc445291f63c96b51a18d5b077875475cf
Zhigang Song <1005411480a@gmail.com>
2014-11-09 12:00:04 +0800

    RBAC和添加用户初步整合
    这里的整合不完善,下次提交RBAC就能做完,到时候继续完善

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -1,26 +1,58 @@
 <?php
 /*
  * 获取Role ID对应的中文标识
- * @param integer $rid Role的ID
+ * @param array $role_list 保存role的数组
+ * @param integer $id Role的ID
  * @return string 角色名称
  */
-function get_role_remark($rid) {
-    // 可以把数据库里面的role缓存起来
-    // 先检测缓存情况, 有缓存就不去读数据库了
-    $rlist = S('role_list');
-
-    if (!array_key_exists($rid, $rlist)) {
-        $db = M('role');
-        $w['rid'] = $rid;
-        $role = $db->where($w)->find();
-        $rlist["$rid"] = $role['remark'];
-
-        // 更新缓存
-        S('role_list', $rlist);
+function get_role_name($id, $role_list = NULL) {
+    if ($role_list == NULL) {
+        $role_list = M('role')->where(array('id' => $id))->select();
     }
 
-    //return $rlist["$rid"];
-    return "abc";
+    foreach ($role_list as $role) {
+        if ($role['id'] == $id) {
+            return $role['remark'];
+        }
+    }
+
+    return "根角色";
+}
+
+/*
+ * 获取Node ID对应的中文标识
+ * @param array $node_list 保存node的数组
+ * @param integer $id node的ID
+ * @return string Node名称
+ */
+function get_node_name($id, $node_list = NULL) {
+    if ($node_list == NULL) {
+        $node_list = M('node')->where(array('id' => $id))->select();
+    }
+
+    foreach ($node_list as $node) {
+        if ($node['id'] == $id) {
+            return $node['title'];
+        }
+    }
+
+    return "根模块";
+}
+
+/*
+ * 检测权限是不是开启了
+ * @param integer $node_id 要检测的结点ID
+ * @param array $access 数据库里面取得的某个用户的权限信息
+ * @return boolean 找到(开启)返回true, 找不到(未开启)返回false
+ */
+function rbac_authority_on($node_id, $access) {
+    foreach ($access as $item) {
+        if ($item['node_id'] == $node_id) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 ?>
--- Application/Admin/Controller/RbacController.class.php
+++ Application/Admin/Controller/RbacController.class.php
@@ -8,27 +8,186 @@ class RbacController extends AdminController {
      * 新加角色
      */
     public function newRole() {
+        if (IS_POST) {
+            $this->doNewRole();
+        } else {
+            $this->pid = I('pid', 0, 'intval');
+            $this->display();
+        }
+    }
+
+    /*
+     * 处理新添角色
+     */
+    private function doNewRole() {
+        $db = M('role');
+        $result = $db->add(I('post.'));
+
+        if ($result) {
+            $this->success('角色新增成功', U('roleList'));
+        } else {
+            $this->error("新增失败, 错误信息:" . $db->getError());
+        }
+    }
+    
+    /*
+     * 角色列表
+     */
+    public function roleList() {
+        $db = M('role');
+        $role_list = $db->select();
+
+        $this->assign("role_list", $role_list);
         $this->display();
+    }
+
+    /*
+     * 编辑角色状态
+     */
+    public function editRole() {
+        if (IS_POST) {
+            $this->doEditRole();
+        } else {
+            $id = I('id');
+
+            $db = M('role');
+            $this->role = $db->where(array('id' => $id))->find();
+
+            $this->display();
+        }
+    }
+
+    /*
+     * 处理角色编辑
+     */
+    private function doEditRole() {
+        $db = M('role');
+        $result = $db->save(I('post.'));
+
+        if ($result) {
+            $this->success('角色更新成功', U('roleList'));
+        } else {
+            $this->error("更新失败, 错误信息:" . $db->getError());
+        }
+    }
+    
+    /*
+     * 配置角色权限
+     */
+    public function roleAccess() {
+        if (IS_POST) {
+            $this->doRoleAccess();
+        } else {
+            $rid = I('id');
+            $adb = M('access');
+            $role_access = $adb->where(array('role_id' => $rid))->select();
+            $ndb = M('node');
+            $node_list = $ndb->field(array('id', 'name', 'title', 'level'))->select();
+
+            $this->assign('rid', $rid);
+            $this->assign('role_access', $role_access);
+            $this->assign('node_list', $node_list);
+            $this->display();
+        }
+    }
+
+    /*
+     * 处理配置权限动作
+     */
+    private function doRoleAccess() {
+        $role_id = I('role_id');
+
+        $access_list = I('access');
+
+        foreach ($access_list as $item) {
+            $info = explode('_', $item);
+
+            $tmp['role_id'] = $role_id;
+            $tmp['node_id'] = $info[0];
+            $tmp['level'] = $info[1];
+
+            $al[] = $tmp;
+        }
+
+        // 删除原来的数据
+        $db = M('access');
+        $db->where(array('role_id' => $role_id))->delete();
+
+        // 插入新加的数据
+        $result = $db->addAll($al);
+        if ($result) {
+            $this->success('编辑权限成功', U('roleList'));
+        } else {
+            $this->error('编辑权限失败');
+        }
     }
 
     /*
      * 新加结点
      */
     public function newNode() {
-        $this->display();
+        if (IS_POST) {
+            $this->doNewNode();
+        } else {
+            $this->pid = I('pid', 0, 'intval');
+            $this->level = I('level', 1, 'intval');
+            $this->display();
+        }
     }
 
     /*
-     * 角色列表
+     * 处理新加结点
      */
-    public function roleList() {
-        $this->display();
+    private function doNewNode() {
+        $db = M('node');
+        $result = $db->add(I('post.'));
+
+        if ($result) {
+            $this->success('新增结点成功', U('nodeList'));
+        } else {
+            $this->error('新增失败, 错误:' . $db->getError());
+        }
     }
 
     /*
      * 结点列表
      */
     public function nodeList() {
+        $db = M('node');
+        $node_list = $db->select();
+
+        $this->assign('node_list', $node_list);
         $this->display();
+    }
+
+    /*
+     * 编辑结点
+     */
+    public function editNode() {
+        if (IS_POST) {
+            $this->doEditNode();
+        } else {
+            $id = I('id');
+
+            $db = M('node');
+            $node = $db->where(array('id' => $id))->find();
+
+            $this->assign('node', $node);
+            $this->display();
+        }
+    }
+
+    /*
+     * 处理编辑结点数据
+     */
+    private function doEditNode() {
+        $db = M('node');
+        $result = $db->save(I('post.'));
+
+        if ($result) {
+            $this->success('新增结点成功', U('nodeList'));
+        } else {
+            $this->error('新增失败, 错误:' . $db->getError());
+        }
     }
 }
